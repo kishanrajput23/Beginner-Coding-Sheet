9f543134b87a323c9fa1d4e19542ceb8636614e5
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-01-23 19:49:11 +0530

    Problems Based on If/Else

--- If-Else/Java/Q_5.java
+++ If-Else/Java/Q_5.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Q_24 {
+public class Q_5 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter year : ");
